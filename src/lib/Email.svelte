46a11ad9fb4ce021d62e7a28c3701d5fc93cdf35
ceci <ceciliafabiole18@gmail.com>
2023-02-13 10:13:00 +0100

    fix:removeItem

--- src/lib/Email.svelte
+++ src/lib/Email.svelte
@@ -4,7 +4,6 @@
 	import TrashCanOutline from 'svelte-material-icons/TrashCanOutline.svelte';
 	import StarOutline from 'svelte-material-icons/StarOutline.svelte';
 	import { createEventDispatcher } from 'svelte';
-	// import { starred } from '$lib/stores/newemail.js';
 
 	const dispatch = createEventDispatcher();
 
@@ -20,6 +19,11 @@
 		dispatch('delete');
 	}
 	//put in special
+	export let special;
+	console.log(special);
+	function putStar() {
+		dispatch('special');
+	}
 </script>
 
 <div
@@ -34,11 +38,9 @@
 	{#if !active}
 		<div class="flex items-center justify-between p-3">
 			{#if !special}
-				<button on:click={handleSpecialEmail}><Icon icon={StarOutline} width="1.5em" /></button>
+				<button on:click={putStar}><Icon icon={StarOutline} width="1.5em" /></button>
 			{:else}
-				<button on:click={handleSpecialEmail}
-					><Icon icon={StarOutline} width="1.5em" color="yellow" /></button
-				>
+				<button on:click={putStar}><Icon icon={StarOutline} width="1.5em" color="yellow" /></button>
 			{/if}
 			<div class="flex w-1/3 items-center justify-between">
 				<p>{email.firstname} {email.lastname}</p>
@@ -56,11 +58,9 @@
 			class="flex h-16 border-spacing-1 cursor-pointer items-center justify-between border border-solid border-gray-400 p-3 shadow-lg"
 		>
 			{#if !special}
-				<button on:click={handleSpecialEmail}><Icon icon={StarOutline} width="1.5em" /></button>
+				<button on:click={putStar}><Icon icon={StarOutline} width="1.5em" /></button>
 			{:else}
-				<button on:click={handleSpecialEmail}
-					><Icon icon={StarOutline} width="1.5em" color="yellow" /></button
-				>
+				<button on:click={putStar}><Icon icon={StarOutline} width="1.5em" color="yellow" /></button>
 			{/if}
 			<div class="flex w-1/3 items-center justify-between">
 				<p>{email.firstname} {email.lastname}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	// @ts-nocheck
 	import Email from '$lib/Email.svelte';
+	// import { starred } from '$lib/stores/newemail.js';
 
 	export let data = {};
 	export let emails = data.emailList;
@@ -11,16 +12,29 @@
 		newEmailList.splice(i, 1);
 		emails = [...newEmailList];
 	}
-	let special = false;
+	export let special = false;
 	function handleSpecialEmail() {
 		special = !special;
+		// 	if (!special) {
+		// 		$starred = [...$starred, email[i]];
+		// 	}
+		// 	if (special) {
+		// 		let newStarred = [...$starred];
+		// 		newStarred.splice(i, 1);
+		// 		$starred = [...newStarred];
+		// 	}
 	}
 </script>
 
 <div>
 	{#if emails.length > 0}
 		{#each emails as email, i}
-			<Email {email} on:delete={() => handleRemove(i)} on:special={handleSpecialEmail(i)} />
+			<Email
+				{email}
+				on:delete={() => handleRemove(i)}
+				on:special={handleSpecialEmail}
+				bind:special
+			/>
 		{/each}
 	{/if}
 </div>
